88851b4823e0feeb563b9d289fe33abcadb76e18
uKnowWhoab1r <md.abir1203@gmail.com>
2023-04-27 04:51:57 +0200

    get_next_line

--- Exam_Rank_03/get_next_line.c
+++ Exam_Rank_03/get_next_line.c
@@ -1,12 +1,5 @@
 #include "get_next_line.h"
 
-/**
- * Saved memory and unnecessary copying by -> line 14
- * Read character one at a time
- * Prevents reading more than the buffer can hold
- * More robust and safe as it checks for allocation,read error
- */
-
 /*
  * To read a book with long sentences, I came up with a way to remember each sentence one at a time. I use a special notebook
  with pages(buffer with malloc) that can only hold a certain number of words.
@@ -20,30 +13,142 @@
  */
 
 
-char *get_next_line(int fd)
-{
-	int i = 0;
-	int reading = 0;
-	char character;
-	char *buffer = malloc(BUFFER_SIZE + 1);
 
-	if (BUFFER_SIZE <= 0 || !buffer)
-		return (NULL);
-	while ((reading = read(fd, &character, 1)) > 0)
-	{
-		buffer[i++] = character;
-		if (character == '\n' || i == BUFFER_SIZE)
-			break;
-	}
-	if (reading == -1)
-	{
-		free(buffer);
-		return (NULL);
-	}
-	buffer[i] = '\0';
-	return (buffer);
+
+
+
+#include "get_next_line.h"
+
+int	ft_strchr(char *str)
+{
+	while (str && *str)
+		if (*str++ == '\n')
+			return (1);
+	return (0);
 }
 
+size_t	ft_strlen(char *str)
+{
+	size_t i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int	i = 0, j = 0;
+	if (!s1)
+	{
+		s1 = malloc(1);
+		*s1 = '\0';
+	}
+	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str)
+		return (0);
+	while (s1[j])
+		str[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}
+
+
+
+
+char	*get_reminder(char *tmp)
+{
+	char *rem;
+	int i = 0, j = 0;
+	while (tmp[i] && tmp[i] != '\n')
+		i++;
+	if (!tmp[i])
+	{
+		free(tmp);
+		return (NULL);
+	}
+	rem = malloc(ft_strlen(tmp) - i);
+	if (!rem)
+		return (NULL);
+	i++;
+	while (tmp[i + j])
+	{
+		rem[j] = tmp[i + j];
+		j++;
+	}
+	rem[j] = '\0';
+	free(tmp);
+	return (rem);
+}
+
+char	*get_new_line(char *tmp)
+{
+	char *line;
+	int	i = 0;
+	while (tmp[i] && tmp[i] != '\n')
+		i++;
+	if (tmp[i] == '\n')
+		i += 1;
+	line = malloc(i + 1);
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (tmp[i] && tmp[i] != '\n')
+	{
+		line[i] = tmp[i];
+		i++;
+	}
+	if (tmp[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*read_file(int fd, char *tmp)
+{
+	char *buff;
+	int	i = 1;
+	buff = malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	while (!ft_strchr(tmp) && i)
+	{
+		i = read(fd, buff, BUFFER_SIZE);
+		if (i == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[i] = '\0';
+		tmp = ft_strjoin(tmp, buff);
+	}
+	free(buff);
+	return (tmp);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*tmp;
+	char *line;
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > UINT_MAX)
+		return (NULL);
+	tmp = read_file(fd, tmp);
+	if (!tmp)
+		return (NULL);
+	if (!(*tmp))
+	{
+		free(tmp);
+		tmp = NULL;
+		return (NULL);
+	}
+	line = get_new_line(tmp);
+	tmp = get_reminder(tmp);
+	return (line);
+}
 
 //
 //#include<stdio.h>
